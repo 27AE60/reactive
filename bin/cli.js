91531d95c41ac2de3b5767163d3f439fe964d29d
SKatiyar <katiyar.suyash@gmail.com>
2016-04-02 13:37:38 +0530

    not sure where is here

--- bin/cli.js
+++ bin/cli.js
@@ -2,16 +2,6 @@
 
 'use strict';
 
-var meow = require('meow');
-var pkg = require('../package.json');
+var argv = require('yargs');
 
-var cli = meow({
-  help: false,
-  pkg: pkg
-});
-
-var opts = cli.flags;
-var args = cli.input;
-var cmd = args[0];
-
-var lib = require('../lib/index.js')(opts, args, cmd);
+console.log(argv);
